ddbaa9ae3abc265e4f8f44f17e81c289f7710087
pepe lopez <plopez741@gmail.com>
2022-04-21 16:04:57 -0700

    added average rating

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,9 @@
 class Movie < ActiveRecord::Base
     has_many :reviews
     has_many :users, through: :reviews
+
+    def average_rating 
+        total_ratings = self.reviews.map(&:rating).sum
+        total_ratings / self.reviews.length
+    end
 end
